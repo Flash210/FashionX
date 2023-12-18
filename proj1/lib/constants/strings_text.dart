5421a5348b6ed68f303ad80e19e8650327ee236c
ReversedFlash <houcem.eddine.mosbah@gmail.com>
2023-12-18 22:27:35 +0100

    commit

--- proj1/lib/constants/strings_text.dart
+++ proj1/lib/constants/strings_text.dart
@@ -1,5 +1,18 @@
+const String appName = "Fashion App";
+const String welcomeHome = "Welcome to the Home Screen!";
 
+// home screen
+const String homeHello = "Hello Dear ";
+const String homeFind = "Find Your Best Product";
+const String homePromo = "30% OFF Winter Collection";
+const String shopNow = "Shop Now";
+const String homeCategories = "Categories";
+const String homeSee = "See All ";
 
+// Item Screen
+const String productName = "Product Name";
+const String productPrice = "\$ 100";
+const String productOldPrice = "\$ 200";
+const String productDesc ="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget velit nec dolor pharetra ferme";
 
-const String APP_NAME = "Fashion App";
-const String Welcome_Home = "Welcome to the Home Screen!";
+const String addToCart = "Add to Cart";
--- proj1/lib/main.dart
+++ proj1/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: APP_NAME,
+      title: appName,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
